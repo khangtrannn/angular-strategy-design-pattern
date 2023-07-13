9a53c4505d2a78923aeddd29d772b5b3649103dd
Khang Tran <khangtrann8198@gmail.com>
2023-07-13 03:20:15 +0000

    Async pipe implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { AsyncPipe, NgForOf } from '@angular/common';
+
+interface User {
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  standalone: true,
+  imports: [AsyncPipe, NgForOf, HttpClientModule],
+  template: `
+    <h1>User list: </h1>
+    <ul>
+      <li *ngFor="let user of users$ | async">{{user.name}}</li>
+    </ul>
+  `,
 })
-export class AppComponent {
-  title = 'angular-strategy-design-pattern';
+export class AppComponent implements OnInit {
+  // users$!: Observable<User[]>;
+  users$!: Promise<User[]>;
+  private http = inject(HttpClient);
+
+  ngOnInit(): void {
+    // this.users$ = this.http.get<User[]>(`https://jsonplaceholder.typicode.com/users`);
+    this.users$ = fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json());
+  }
 }
